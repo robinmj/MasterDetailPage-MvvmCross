9d5bf35ca6e24a0ed39473d454d6372aa04b820c
Robin Johnson <robin.m.j@gmail.com>
2016-01-09 15:40:17 -0700

    added splash screen

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -17,7 +17,7 @@ using Cirrious.CrossCore;
 
 namespace MasterDetailPageNavigation.Droid
 {
-	[Activity (Label = "MasterDetailPageNavigation.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "MasterDetailPageNavigation.Droid", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : FormsApplicationActivity
 	{
 		protected override void OnCreate(Bundle bundle)
